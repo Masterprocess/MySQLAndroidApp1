7d6515fa8048804d4a0ea250afd0a23b3e4260ad
masterprocess <misterrackley@gmail.com>
2018-03-19 21:00:08 -0400

    layout finished

---
+++ app/src/main/java/DBStrings.java
@@ -0,0 +1,14 @@
+import java.net.URL;
+
+/**
+ * Created by masterprocess on 3/19/18.
+ */
+
+public class DBStrings {
+
+    static final String DATABASE_URL = "127.0.0.1:3306";
+    static final String DATABASE_NAME = "mhs_example";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "root";
+
+}
